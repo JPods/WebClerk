98384f5b05d174b35534e353f9716c9559b9d6f2
An7orAhmed <antor.mee@gmail.com>
2025-05-25 20:26:15 +0600

    sql added

---
+++ core/tables.sql
@@ -0,0 +1,78 @@
+CREATE TABLE IF NOT EXISTS "contacts" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "uuid" VARCHAR(36) NOT NULL UNIQUE,
+    "email" VARCHAR(254) NOT NULL UNIQUE,
+    "password" VARCHAR(128) NOT NULL,
+    "verification_code" VARCHAR(100),
+    "verification_code_expiry" TIMESTAMP WITH TIME ZONE,
+    "is_email_verified" BOOLEAN NOT NULL DEFAULT FALSE,
+    "attention" VARCHAR(255),
+    "comment_alert" VARCHAR(255),
+    "company" VARCHAR(255),
+    "name_first" VARCHAR(50) NOT NULL,
+    "name_last" VARCHAR(50) NOT NULL,
+    "name_middle" VARCHAR(50),
+    "opt_out" VARCHAR(255),
+    "prefix" VARCHAR(50),
+    "publish" INTEGER,
+    "rank" VARCHAR(50),
+    "salutation" VARCHAR(50),
+    "suffix" VARCHAR(50),
+    "role" TEXT[] NOT NULL DEFAULT '{}',
+    "comment" TEXT,
+    "refs" JSONB NOT NULL DEFAULT '{}',
+    "metadata" JSONB NOT NULL DEFAULT '{}',
+    "is_active" BOOLEAN NOT NULL DEFAULT TRUE,
+    "is_staff" BOOLEAN NOT NULL DEFAULT FALSE,
+    "date_joined" TIMESTAMP WITH TIME ZONE NOT NULL,
+    "is_superuser" BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS "actions" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "uuid" UUID UNIQUE NOT NULL,
+    "action" VARCHAR(255),
+    "action_by" VARCHAR(255),
+    "comment" TEXT,
+    "description" VARCHAR(255),
+    "difficulty" VARCHAR(255),
+    "dt_action" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "dt_completed" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "dt_due" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "dt_updated" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "hours" DOUBLE PRECISION,
+    "metadata" JSONB,
+    “refs” JSONB,
+    "percent" INTEGER,
+    "priority" VARCHAR(255),
+    "quality" VARCHAR(255),
+    "references" JSONB,
+    "status" VARCHAR(255)
+);
+
+
+CREATE TABLE IF NOT EXISTS "templates" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "uuid" UUID UNIQUE NOT NULL,
+    "comment" TEXT,
+    "metadata" JSONB,
+    "name" VARCHAR(255),
+    "purpose" VARCHAR(255),
+    "references" JSONB,
+    "table_name" VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS "settings" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "uuid" UUID UNIQUE NOT NULL,
+    "comment" TEXT,
+    "is_active" BOOLEAN DEFAULT FALSE,
+    "metadata" JSONB,
+    "name" VARCHAR(255),
+    "purpose" VARCHAR(255),
+    "references" JSONB,
+    "role" VARCHAR(255),
+    "security_level" INTEGER,
+    "table_name" VARCHAR(255)
+);
+
