6bd84bf3d5be4f085ee34cf803f3c55659d54675
JPods <bill.james@jpods.com>
2025-08-03 08:04:15 -0500

    draft wcapi

--- core/services/wcapi.py
+++ core/services/wcapi.py
@@ -13,8 +13,67 @@ MODEL_MAP = {
 }
 
 class UniversalGetView(View):
+    def connect(self, request):
+        table_name = request.META.get('HTTP_TABLE_NAME')
+        model = MODEL_MAP.get(table_name)
+        if not model:
+            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
+        data = list(model.objects.all().values())
+        return JsonResponse({'success': True, 'data': data})
+
+    def delete(self, request):
+        table_name = request.DELETE.get('table_name')
+        model = MODEL_MAP.get(table_name)
+        if not model:
+            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
+        data = list(model.objects.all().values())
+        return JsonResponse({'success': True, 'data': data})
+    
     def get(self, request):
         table_name = request.GET.get('table_name')
+        model = MODEL_MAP.get(table_name)
+        if not model:
+            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
+        data = list(model.objects.all().values())
+        return JsonResponse({'success': True, 'data': data})
+
+    def head(self, request):
+        table_name = request.META.get('HTTP_TABLE_NAME')
+        model = MODEL_MAP.get(table_name)
+        if not model:
+            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
+        data = list(model.objects.all().values())
+        return JsonResponse({'success': True, 'data': data})    
+    # Help page for WCapi - could be a static page, duplicate of options and root? QQQ
+    def help(self, request):
+        return JsonResponse({'success': True, 'message': 'Help page for WCapi'})
+
+    def manage(self, request):
+        table_name = request.GET.get('table_name') or request.POST.get('table_name')
+        model = MODEL_MAP.get(table_name)
+        if not model:
+            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
+        data = list(model.objects.all().values())
+        return JsonResponse({'success': True, 'data': data})
+
+    def options(self, request):
+        table_name = request.META.get('HTTP_TABLE_NAME')
+        model = MODEL_MAP.get(table_name)
+        if not model:
+            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
+        data = list(model.objects.all().values())
+        return JsonResponse({'success': True, 'data': data})        
+
+    def patch(self, request):
+        table_name = request.PATCH.get('table_name')
+        model = MODEL_MAP.get(table_name)
+        if not model:
+            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
+        data = list(model.objects.all().values())
+        return JsonResponse({'success': True, 'data': data})    
+    
+    def post(self, request):
+        table_name = request.POST.get('table_name')
         model = MODEL_MAP.get(table_name)
         if not model:
             return JsonResponse({'success': False, 'message': 'Invalid table_name'})
@@ -28,17 +87,9 @@ class UniversalGetView(View):
             return JsonResponse({'success': False, 'message': 'Invalid table_name'})
         data = list(model.objects.all().values())
         return JsonResponse({'success': True, 'data': data})
-    
-    def post(self, request):
-        table_name = request.POST.get('table_name')
-        model = MODEL_MAP.get(table_name)
-        if not model:
-            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
-        data = list(model.objects.all().values())
-        return JsonResponse({'success': True, 'data': data})
-    
-    def delete(self, request):
-        table_name = request.DELETE.get('table_name')
+
+    def trace(self, request):
+        table_name = request.META.get('HTTP_TABLE_NAME')
         model = MODEL_MAP.get(table_name)
         if not model:
             return JsonResponse({'success': False, 'message': 'Invalid table_name'})
@@ -46,4 +97,10 @@ class UniversalGetView(View):
         return JsonResponse({'success': True, 'data': data})
     
     
+    
+
+
+
+
+# Similar implementations would be needed for UniversalCRUDView, UniversalQueryView, UniversalSaveView, and UniversalDeleteView
     
