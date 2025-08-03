e8c54d81afb7c6515c00a8c7fda617c44385edf4
JPods <bill.james@jpods.com>
2025-08-03 07:09:49 -0500

    wcapi basics

--- core/services/wcapi.py
+++ core/services/wcapi.py
@@ -1,10 +1,14 @@
 from django.views import View
 from django.http import JsonResponse
-from core.models import Contact, Action  # Import only allowed models
+from core.models import Contact, Action, Phone, Domain, Email, Address  # Import only allowed models
 
 MODEL_MAP = {
     'contacts': Contact,
     'actions': Action,
+    'phones': Phone,
+    'domains': Domain,
+    'emails': Email,
+    'addresses': Address,
     # Add other allowed models
 }
 
@@ -16,3 +20,30 @@ class UniversalGetView(View):
             return JsonResponse({'success': False, 'message': 'Invalid table_name'})
         data = list(model.objects.all().values())
         return JsonResponse({'success': True, 'data': data})
+    
+    def put(self, request):
+        table_name = request.PUT.get('table_name')
+        model = MODEL_MAP.get(table_name)
+        if not model:
+            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
+        data = list(model.objects.all().values())
+        return JsonResponse({'success': True, 'data': data})
+    
+    def post(self, request):
+        table_name = request.POST.get('table_name')
+        model = MODEL_MAP.get(table_name)
+        if not model:
+            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
+        data = list(model.objects.all().values())
+        return JsonResponse({'success': True, 'data': data})
+    
+    def delete(self, request):
+        table_name = request.DELETE.get('table_name')
+        model = MODEL_MAP.get(table_name)
+        if not model:
+            return JsonResponse({'success': False, 'message': 'Invalid table_name'})
+        data = list(model.objects.all().values())
+        return JsonResponse({'success': True, 'data': data})
+    
+    
+    
