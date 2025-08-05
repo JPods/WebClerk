5b8f5dc0724791d60cf8e6ac7ca9b5761773c772
JPods <bill.james@jpods.com>
2025-08-04 21:58:24 -0500

    Postman endpoints

--- core/urls.py
+++ core/urls.py
@@ -26,13 +26,10 @@ urlpatterns = [
     # Rare for sockets. We will more likely create an api app for websockets
     
 
-        path('wcapi/connect/', UniversalCRUDView.as_view(template_name='core/connect.html'), name='connect'),
+    path('wcapi/connect/', UniversalCRUDView.as_view(template_name='core/connect.html'), name='connect'),
     
     # delete
     path('wcapi/delete/', UniversalCRUDView.as_view(), name='delete-crud'),
-    
-    # list/read records
-    path('wcapi/query/', UniversalCRUDView.as_view(), name='query'),
     
     # list/read records
     path('wcapi/get/', UniversalCRUDView.as_view(), name='get'),
@@ -43,6 +40,10 @@ urlpatterns = [
     # Help page for wcapi - could be a static page, duplicate of options and root? QQQ
     path('wcapi/help/', UniversalCRUDView.as_view(), name='core/help.html'),
     
+    # list/read records
+    path('wcapi/keywords/', UniversalCRUDView.as_view(), name='keywords'),
+
+
     # do not know
     path('wcapi/manage/', UniversalCRUDView.as_view(), name='manage'),
     
@@ -55,8 +56,12 @@ urlpatterns = [
     # create or full update
     path('wcapi/post/', UniversalCRUDView.as_view(), name='post'),
     
+    path('wcapi/query/', UniversalCRUDView.as_view(), name='query'),
+
     # full update
-    path('wcapi/put', UniversalCRUDView.as_view(), name='put'),
+    path('wcapi/save/', UniversalCRUDView.as_view(), name='save'),
+ 
+    path('wcapi/put/', UniversalCRUDView.as_view(), name='put'),
     # save was used in 4D, replace with post/put
     # path('wcapi/save/', UniversalSaveView.as_view(), name='save'),
     # Root endpoint for wcapi
