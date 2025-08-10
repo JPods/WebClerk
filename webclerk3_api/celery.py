ef22f43332394841f21af69baa02780aa6abf7dc
JPods <bill.james@jpods.com>
2025-08-10 09:29:35 -0500

    missing webclerk3_api docs

---
+++ webclerk3_api/celery.py
@@ -0,0 +1,12 @@
+import os
+from celery import Celery
+
+# 3 terminals named celery, redis, python
+# % redis-server
+# % celery -A webclerk3_api worker -l info
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webclerk3_api.settings')
+
+app = Celery('webclerk3_api')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
