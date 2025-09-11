045bca42574d35ea4e0b0c6d7636f59b516720f8
JPods <bill.james@jpods.com>
2025-09-10 23:58:56 -0500

    Frontend examples

---
+++ frontend-endpoint-explorer/src/main.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Explorer } from './components/Explorer';
+
+createRoot(document.getElementById('root') as HTMLElement).render(<Explorer />);
