f48935909b32788288b6e43df782c261a3e7b71a
JPods <bill.james@jpods.com>
2025-11-13 13:01:30 -0600

    Remove /local folder

--- frontend-endpoint-explorer/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Explorer } from './components/Explorer';
-
-createRoot(document.getElementById('root') as HTMLElement).render(<Explorer />);
