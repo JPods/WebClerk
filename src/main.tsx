17986b0688adc3c9378278fa15df31dfb1f72557
Riju Karar <technoriju@gmail.com>
2025-12-23 23:54:31 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.scss";
+import "./index.css";
 import "swiper/swiper-bundle.css";
 import "flatpickr/dist/flatpickr.css";
 import App from "./App.tsx";
