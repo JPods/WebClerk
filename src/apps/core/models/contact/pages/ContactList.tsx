df53f21051660c0577838340301e4aa37366627f
Samir Biswas <samir@gmail.com>
2025-12-24 14:41:11 +0530

    dark theme fixed for contact

--- src/apps/core/models/contact/pages/ContactList.tsx
+++ src/apps/core/models/contact/pages/ContactList.tsx
@@ -254,7 +254,6 @@ export default function ContactList() {
                   </span>
                   <input
                     type="text"
-                    value={filteredSearch}
                     onChange={(e) => filterData(e.target.value)}
                     placeholder="Search for record..."
                     className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-200 bg-transparent py-2.5 pl-12 pr-14 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-800 dark:bg-gray-900 dark:bg-white/[0.03] dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 "
@@ -282,7 +281,7 @@ export default function ContactList() {
                 </button>
               </div>
             </div>
-            <div className="w-full overflow-x-auto rounded-md cus-bg-purple-light dark:bg-gray-900 h-[calc(100vh-265px)]">
+            <div className="w-full overflow-x-auto rounded-md cus-bg-purple-light dark:!bg-[#1e2636] dark:bg-gray-900 h-[calc(100vh-265px)]">
               {isMobile || formMode ? (
                 <div className="flex flex-col">
                   <ContactListMob
