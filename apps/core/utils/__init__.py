77823b86e2ece36d0c0bc92150d585159ebc120f
An7orAhmed <antorahmed.se@gmail.com>
2026-01-13 00:35:05 +0600

    import fixed

--- apps/core/utils/__init__.py
+++ apps/core/utils/__init__.py
@@ -1,30 +1,36 @@
 # Core utility modules
 
-# Import commonly used utilities for convenience
-from ..management.commands.misc.misc import (
-    validate_filename_length,
-    sanitize_filename,
-    generate_barcode_data,
-    extract_keywords_from_text,
-    format_currency_amount,
-    calculate_percentage_change,
-    safe_divide,
-    truncate_text,
-    merge_dicts,
-    WebClerk2Utils,
-)
+# Import commonly used utilities for convenience (tolerant if misc module is absent)
+try:
+    from ..management.commands.misc.misc import (
+        validate_filename_length,
+        sanitize_filename,
+        generate_barcode_data,
+        extract_keywords_from_text,
+        format_currency_amount,
+        calculate_percentage_change,
+        safe_divide,
+        truncate_text,
+        merge_dicts,
+        WebClerk2Utils,
+    )
+    _MISC_EXPORTS = [
+        'validate_filename_length',
+        'sanitize_filename',
+        'generate_barcode_data',
+        'extract_keywords_from_text',
+        'format_currency_amount',
+        'calculate_percentage_change',
+        'safe_divide',
+        'truncate_text',
+        'merge_dicts',
+        'WebClerk2Utils',
+    ]
+except Exception:
+    _MISC_EXPORTS = []
+
 from .policy import get_accessible_fields
 
-__all__ = [
-    'validate_filename_length',
-    'sanitize_filename',
-    'generate_barcode_data',
-    'extract_keywords_from_text',
-    'format_currency_amount',
-    'calculate_percentage_change',
-    'safe_divide',
-    'truncate_text',
-    'merge_dicts',
-    'WebClerk2Utils',
+__all__ = _MISC_EXPORTS + [
     'get_accessible_fields',
 ]
