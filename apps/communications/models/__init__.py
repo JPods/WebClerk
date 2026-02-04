69310ac981d3cad6b35b680bcc89427f99c8e01a
JPods <bill.james@jpods.com>
2026-02-03 19:49:10 -0600

    cleaned up Location to Address

--- apps/communications/models/__init__.py
+++ apps/communications/models/__init__.py
@@ -1,8 +1,10 @@
 # path: apps/communications/models/__init__.py
 from .address import Address
-from .location import Location  # deprecated alias; import Address instead
 from .email import Email
 from .phone import Phone
 from .domain import Domain
+
+# Deprecated alias: Location was renamed to Address
+Location = Address
 
 __all__ = ["Address", "Location", "Email", "Phone", "Domain"]
