98b3d7b0bd4c24106d5e82a2229cc3c0e1023837
Riju Karar <technoriju@gmail.com>
2026-02-08 20:04:52 +0530

    update emplyee

--- src/apps/orgs/models/employee/utils/employeeSchema.ts
+++ src/apps/orgs/models/employee/utils/employeeSchema.ts
@@ -2,8 +2,8 @@ import * as z from "zod";
 
 export const employeeSchema = z.object({
   display_name: z.string().min(1, "Name is required"),
-  org_type: z.string().default("customer"),
-  status: z.string().min(1, "Phone is required"),
-  version: z.number().default(1),
+  org_type: z.string().default("Employee").optional(),
+  status: z.string().min(1, "Status is required"),
+  version: z.number().default(1).optional(),
   is_active: z.boolean().default(false),
 });
